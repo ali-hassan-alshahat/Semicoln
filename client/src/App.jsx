709db302c595134d1880f4fb40020cd285494baa
Ali Hassan <ali.hassan.alshahat@gmail.com>
2026-05-11 17:41:47 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="bg-error">Hello world</div>
+      <Button variant="success"> Mark As Done </Button>
+    </>
+  );
+}
+
+export default App;
